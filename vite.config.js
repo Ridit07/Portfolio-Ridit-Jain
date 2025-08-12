7f9d236b6b6451e6e53e32f88648f07f78f514b7
Ridit Jain <112847735+Ridit07@users.noreply.github.com>
2025-08-13 01:04:41 +0530

    temp changed outdir

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     open: false
   },
   build: {
-    outDir: 'dist',
+    outDir: 'build',
     sourcemap: false, // Disable sourcemaps for prod
     chunkSizeWarningLimit: 1000
   }

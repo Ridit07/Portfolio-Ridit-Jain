cfab931955d84a70926c1eb62b92371fe15d233e
Ridit Jain <112847735+Ridit07@users.noreply.github.com>
2025-08-16 00:13:58 +0530

    cleanup

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   build: {
     outDir: 'build',
-    sourcemap: false, // Disable sourcemaps for prod
+    sourcemap: false, 
     chunkSizeWarningLimit: 1000
   }
 });
